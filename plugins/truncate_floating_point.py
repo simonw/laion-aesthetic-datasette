5c0295b395328ace0e2114b9702ce2fa5366ed91
Simon Willison <swillison@gmail.com>
2022-08-30 14:18:30 -0700

    Try to avoid 1.3890318e-07 syntax, refs #1

--- plugins/truncate_floating_point.py
+++ plugins/truncate_floating_point.py
@@ -4,4 +4,8 @@ from datasette import hookimpl
 @hookimpl
 def render_cell(column, value):
     if isinstance(value, float):
-        return round(value, 5)
+        # Try to avoid confusing 1.3890318e-07 syntax
+        if value < 0.0001:
+            return "{:.10f}".format(value)
+        else:
+            return round(value, 5)
