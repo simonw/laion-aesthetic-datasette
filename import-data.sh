117e44a24f0b486f3b45fbda239254bdfd4c8dac
Simon Willison <swillison@gmail.com>
2022-08-30 13:40:31 -0700

    laion-aesthetic-6pls.db
    Refs #7

--- import-data.sh
+++ import-data.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Create table
-sqlite3 laion-aesthetic.db '
+sqlite3 laion-aesthetic-6pls.db '
 CREATE TABLE IF NOT EXISTS images (
-   [text] TEXT,
    [url] TEXT,
+   [text] TEXT,
    [width] INTEGER,
    [height] INTEGER,
    [similarity] FLOAT,
-   [hash] TEXT,
    [punsafe] FLOAT,
    [pwatermark] FLOAT,
-   [aesthetic] FLOAT
+   [aesthetic] FLOAT,
+   [hash] TEXT,
+   [__index_level_0__] INTEGER
 );'
 
 # Loop through and import ever *.parquet file in the current directory
 for filename in *.parquet; do
-    parquet-tools csv $filename | sqlite3 -csv laion-aesthetic.db ".import --skip 1 '|cat -' images"
+    parquet-tools csv $filename | sqlite3 -csv laion-aesthetic-6pls.db ".import --skip 1 '|cat -' images"
 done
