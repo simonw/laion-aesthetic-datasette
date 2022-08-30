f9103394e0079dc22c44fb9d639a1f1a31b3eb66
Simon Willison <swillison@gmail.com>
2022-08-30 13:59:57 -0700

    Better floating point truncation, refs #11

--- plugins/truncate_floating_point.py
+++ plugins/truncate_floating_point.py
@@ -4,5 +4,4 @@ from datasette import hookimpl
 @hookimpl
 def render_cell(column, value):
     if isinstance(value, float):
-        # Truncate to 4 decimal places
-        return "{0:.4g}".format(value)
+        return round(value, 5)
